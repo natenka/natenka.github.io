39ec8b84c6f8ee8add81aa78b42b335edca4cef2
pyneng <pyneng.course@gmail.com>
2020-04-06 21:14:56 +0000

    pytest example

---
+++ code_examples/pytest_network_test/testcode_ver2.py
@@ -0,0 +1,47 @@
+import yaml
+from netmiko import ConnectHandler
+import pytest
+import pytest_check as check
+
+
+with open('devices.yaml') as f:
+    devices = yaml.safe_load(f)
+
+
+@pytest.fixture(scope="module")
+def routers_from_devices_yaml():
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+        #options = {"timeout": 5, "fast_cli": True}
+        options = {"timeout": 5}
+        for device in devices:
+            device.update(options)
+    return devices
+
+
+@pytest.fixture(scope="module")
+def routers_test_connection(routers_from_devices_yaml):
+    connections = {}
+    for params in routers_from_devices_yaml:
+        ssh = ConnectHandler(**params)
+        ssh.enable()
+        connections[params["host"]] = ssh
+    yield connections
+    for r in connections.values():
+        r.disconnect()
+
+
+device_route_map = {
+    "192.168.100.1": ['1.1.1.1', '2.2.2.1', '4.4.4.4'],
+    "192.168.100.2": ['1.1.1.1'],
+}
+
+
+@pytest.mark.parametrize(
+    'device, route',
+    [(device, route) for device, routes in device_route_map.items() for route in routes]
+)
+def test_ssh(routers_test_connection, device, route):
+    ssh = routers_test_connection[device]
+    result = ssh.send_command('sh ip route')
+    assert route in result
---
+++ code_examples/pytest_network_test/testcode_ver3.py
@@ -0,0 +1,45 @@
+import yaml
+from netmiko import ConnectHandler
+import pytest
+import pytest_check as check
+
+
+with open('devices.yaml') as f:
+    devices = yaml.safe_load(f)
+
+
+device_route_map = {
+    "192.168.100.1": ['1.1.1.1', '2.2.2.1', '4.4.4.4'],
+    "192.168.100.2": ['1.1.1.1'],
+}
+
+
+@pytest.fixture(scope="module")
+def routers_connection():
+    routers = device_route_map.keys()
+    params = {
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+        "device_type": "cisco_ios",
+    }
+    connections = {}
+    for ip in routers:
+        params["host"] = ip
+        ssh = ConnectHandler(**params)
+        ssh.enable()
+        connections[ip] = ssh
+    yield connections
+    for r in connections.values():
+        r.disconnect()
+
+
+@pytest.mark.parametrize(
+    'device, route',
+    [(device, route) for device, routes in device_route_map.items() for route in routes]
+)
+def test_ssh(routers_connection, device, route):
+    ssh = routers_connection[device]
+    result = ssh.send_command('sh ip route')
+    assert route in result
+
