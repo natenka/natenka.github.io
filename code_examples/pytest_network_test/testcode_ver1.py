770b90836b03e3e93bf53b93f55aff3bba17e3c8
pyneng <pyneng.course@gmail.com>
2020-04-07 05:35:09 +0000

    pytest example

--- code_examples/pytest_network_test/testcode_ver1.py
+++ code_examples/pytest_network_test/testcode_ver1.py
@@ -4,7 +4,7 @@ import pytest
 import pytest_check as check
 
 
-with open('devices.yaml') as f:
+with open("devices.yaml") as f:
     devices = yaml.safe_load(f)
 
 
@@ -15,12 +15,16 @@ def send_show_command(device, command):
 
 
 @pytest.mark.parametrize(
-    'device, route',
-    [*((devices[0], route) for route in ('1.1.1.1', '2.2.2.1', '4.4.4.4')),
-     (devices[1], '1.1.1.1')]
+    "device, route",
+    [
+        *((devices[0], route) for route in ("1.1.1.1", "2.2.2.1", "4.4.4.4")),
+        (devices[1], "1.1.1.1"),
+    ],
 )
 def test_check_route(device, route):
-    device.update({'fast_cli': True})
-    command = 'show ip route'
+    device.update({"fast_cli": True})
+    command = "show ip route"
     output = send_show_command(device, command)
-    assert route in output, f'Маршрут {route} отсутствует на устройстве {device["host"]}'
+    assert (
+        route in output
+    ), f'Маршрут {route} отсутствует на устройстве {device["host"]}'
--- code_examples/pytest_network_test/testcode_ver2.py
+++ code_examples/pytest_network_test/testcode_ver2.py
@@ -4,7 +4,7 @@ import pytest
 import pytest_check as check
 
 
-with open('devices.yaml') as f:
+with open("devices.yaml") as f:
     devices = yaml.safe_load(f)
 
 
@@ -12,7 +12,7 @@ with open('devices.yaml') as f:
 def routers_from_devices_yaml():
     with open("devices.yaml") as f:
         devices = yaml.safe_load(f)
-        #options = {"timeout": 5, "fast_cli": True}
+        # options = {"timeout": 5, "fast_cli": True}
         options = {"timeout": 5}
         for device in devices:
             device.update(options)
@@ -32,16 +32,20 @@ def routers_test_connection(routers_from_devices_yaml):
 
 
 device_route_map = {
-    "192.168.100.1": ['1.1.1.1', '2.2.2.1', '4.4.4.4'],
-    "192.168.100.2": ['1.1.1.1'],
+    "192.168.100.1": ["1.1.1.1", "2.2.2.1", "4.4.4.4"],
+    "192.168.100.2": ["1.1.1.1"],
 }
 
 
 @pytest.mark.parametrize(
-    'device, route',
-    [(device, route) for device, routes in device_route_map.items() for route in routes]
+    "device, route",
+    [
+        (device, route)
+        for device, routes in device_route_map.items()
+        for route in routes
+    ],
 )
 def test_ssh(routers_test_connection, device, route):
     ssh = routers_test_connection[device]
-    result = ssh.send_command('sh ip route')
+    result = ssh.send_command("sh ip route")
     assert route in result
